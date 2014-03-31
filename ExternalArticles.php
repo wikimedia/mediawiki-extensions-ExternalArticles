5e168557b04680909e4becbdb2e2aa88450d1ec6
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-31 14:39:43 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ieab4aecd8c69fd869e0c2c95a519815ff7533a3b

--- ExternalArticles.php
+++ ExternalArticles.php
@@ -1,31 +1,30 @@
 <?php
 /**
  * ExternalArticles.php
- * 
+ *
  * The ExternalArticles extension fetches pages from a remote wiki.
- * 
+ *
  * Copyright (C) 2009-2013 the authors listed below.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
- * 
+ *
  * @ingroup Extensions
  * @author Nathan Perry <externalarticles@nateperry.org>
  * @author Alvinos http://www.mediawiki.org/wiki/User:Alvinos
  * @author Sam Wilson <sam@samwilson.id.au>
- * @version 0.1.4
  * @link http://www.nateperry.org/wiki/External_Articles
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @file
@@ -53,24 +52,25 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'External Articles',
 	'descriptionmsg' => 'externalarticles-desc',
-	'version' => '0.1.4', // version date 2013-05-31
+	'version' => '0.2.0', // version date 2014-03-31
 	'author' => array( 'Nathan Perry', 'Alvinos', 'Sam Wilson' ),
 	'url' => 'http://www.mediawiki.org/wiki/Extension:ExternalArticles'
 );
 
+$wgMessagesDirs['ExternalArticles'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['ExternalArticles'] = __DIR__ . '/ExternalArticles.i18n.php';
 
-// todo: change this so each setting is set to it's default if it is not defined.
+// @todo: change this so each setting is set to it's default if it is not defined.
 //       Currently, if anything is overridden, all must be defined.
 if ( !isset( $eagRules ) || is_null( $eagRules ) ) {
 	$eagRules = array();
 	$eagRules['onpreload'] = true;
 	$eagRules['url'] = 'http://en.wikipedia.org/w/index.php?title=';
 
-	// todo: remove assumption of English.
+	// @todo: remove assumption of English.
 	$eagRules['rule'] = '/^Template:.*$/'; // http://us3.php.net/manual/en/function.preg-match.php
 } else {
-	// todo: validate $eagRules URL's, etc...
+	// @todo: validate $eagRules URL's, etc...
 }
 
 $wgHooks['EditFormPreloadText'][] = 'ExternalArticles_EditFormPreloadText';
@@ -78,7 +78,7 @@ $wgHooks['EditFormPreloadText'][] = 'ExternalArticles_EditFormPreloadText';
 /**
  * Preload text from a remote wiki into the edit form. Called when edit page for
  * a new article is shown.
- * 
+ *
  * @global OutputPage $wgOut OutputPage object for HTTP response
  * @global array $eagRules ExternalArticles configuration array
  * @param string $text Text with which to prefill the edit form
