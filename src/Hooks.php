e32376f3513594f5d87bf5f5d476503ca9174047
Sam Wilson <sam@samwilson.id.au>
2017-02-20 15:19:47 +0800

    Convert to extension registration
    Add extension.json and refactor the main code into a Hooks class.
    
    Change-Id: I5de7bd3d9779bd6bd8452ea48076abf613c67099

---
+++ src/Hooks.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace MediaWiki\Extensions\ExternalArticles;
+
+use MWHttpRequest;
+use OutputPage;
+use Title;
+
+class Hooks {
+
+	/**
+	 * Preload text from a remote wiki into the edit form. Called when edit page for
+	 * a new article is shown.
+	 *
+	 * @global OutputPage $wgOut OutputPage object for HTTP response
+	 * @global array $eagRules ExternalArticles configuration array
+	 * @param string &$text Text with which to prefill the edit form
+	 * @param Title &$title Title of the new page
+	 * @return boolean
+	 */
+	function onEditFormPreloadText( &$text, Title &$title ) {
+		global $wgOut, $eagRules;
+
+		// @todo: change this so each setting is set to it's default if it is not defined.
+		// Currently, if anything is overridden, all must be defined.
+		if ( !isset( $eagRules ) || is_null( $eagRules ) ) {
+			$eagRules = [];
+			$eagRules['onpreload'] = true;
+			$eagRules['url'] = 'https://en.wikipedia.org/w/index.php?title=';
+
+			// @todo: remove assumption of English.
+			$eagRules['rule'] = '/^Template:.*$/';
+		} else {
+			// @todo: validate $eagRules URL's, etc...
+		}
+
+		$pagename = $title->getPrefixedURL();
+		$url = $eagRules['url'] . $pagename . '&action=raw';
+		$ismatch = preg_match( $eagRules['rule'], $pagename ) > 0;
+
+		if ( defined( 'EXTERNALARTICLES_DEBUG' ) ) {
+			if ( $ismatch ) {
+				$wgOut->addWikiText( "URL: $url<br />" );
+			} else {
+				$wgOut->addWikiText( "Page title does not match rule.<br />" );
+			}
+		}
+
+		if ( $eagRules['onpreload'] && $ismatch && empty( $text ) ) {
+			$options = [
+				'followRedirects' => true,
+			];
+			$httpRequest = MWHttpRequest::factory( $url, $options );
+			$status = $httpRequest->execute();
+			if ( !$status->isOK() ) {
+				if ( defined( 'EXTERNALARTICLES_DEBUG' ) ) {
+					$wgOut->addWikiText( "Failed to fetch external page: " . $status->getWikiText() );
+				}
+
+				return false;
+			}
+			$wgOut->wrapWikiMsg( '<div class="success">$1</div>',
+				[ 'externalarticles-article-loaded', $url ] );
+			$text = $httpRequest->getContent();
+
+			return true;
+		}
+
+		return true;
+	}
+}
